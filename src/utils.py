acde5fb4a982c45c3c39a3f1f4a61a4173fc97a0
artchemi <art.dan08@yandex.ru>
2025-06-05 14:28:25 +0800

    UPDATE window generator

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,15 @@
 import numpy as numpy
 
 
-def split_into_batches(data, batch_size: int) -> list:
-    """Разделяет всю серию ЭМГ или меток по батчам
+def split_into_batches(data, window_size: int, step: int) -> list:
+    """Разделяет всю серию ЭМГ или меток по окнам
 
     Args:
         data (_type_): ЭМГ или метки
-        batch_size (int): размер окна
+        window_size (int): размер окна
 
     Returns:
         list: Список батчей
     """
-    return [data[i:i + batch_size] for i in range(0, len(data), batch_size)]
+    # return [data[i:i + window_size] for i in range(0, len(data), window_size)]
+    return [data[i:i + window_size] for i in range(0, len(data) - window_size + 1, step)]
